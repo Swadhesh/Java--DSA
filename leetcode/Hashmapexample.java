ad477b223513555cd4062cf6e5d3cd06426e4b38
Swadhesh <75163426+Swadhesh@users.noreply.github.com>
2022-09-10 20:14:29 +0530

    init

---
+++ leetcode/Hashmapexample.java
@@ -0,0 +1,12 @@
+import java.util.HashMap;
+public class Hashmapexample{
+    public static void main(String[] args){
+        HashMap<String,Integer> hmap=new HashMap<String,Integer>();
+        hmap.put("swadhesh",108);
+        hmap.put("manoj",51);
+        hmap.put("saru",89);
+        
+        System.out.println(hmap.);
+        System.out.println(hmap);
+    }
+}
